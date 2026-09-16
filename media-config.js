9515a7b932faab470d40c6871db75ea8b6acbfaf
fodiltrader-blip <fodiltrader@gmail.com>
2026-09-16 19:50:32 +0100

    Render AdsPower lesson with localized R2 screenshots
    * Add localized AdsPower R2 media paths
    
    * Render AdsPower lesson with localized R2 screenshots
    
    * Load AdsPower lesson runtime

--- media-config.js
+++ media-config.js
@@ -5,12 +5,26 @@ window.GuideFlowMedia = Object.freeze({
     ar: Object.freeze({
       iproyalOverview: '/images/iproyal/ar/residential-interface.png',
       iproyalSettings: '/images/iproyal/ar/proxy-settings.png',
-      iproyalList: '/images/iproyal/ar/proxy-list.png'
+      iproyalList: '/images/iproyal/ar/proxy-list.png',
+      adspowerCopyProxy: '/images/adspower/ar/01-iproyal-copier-proxy.png',
+      adspowerOpenProxies: '/images/adspower/ar/02-section-proxies.png',
+      adspowerPasteProxy: '/images/adspower/ar/03-coller-proxy.png',
+      adspowerImportedProxy: '/images/adspower/ar/04-proxy-importe.png',
+      adspowerLinkProfile: '/images/adspower/ar/05-lier-proxy-profil.png',
+      adspowerChooseSaved: '/images/adspower/ar/06-choisir-saved-proxy.png',
+      adspowerVerifyProxy: '/images/adspower/ar/07-verification-proxy.png'
     }),
     fr: Object.freeze({
       iproyalOverview: '/images/iproyal/fr/residential-interface.png',
       iproyalSettings: '/images/iproyal/fr/proxy-settings.png',
-      iproyalList: '/images/iproyal/fr/proxy-list.png'
+      iproyalList: '/images/iproyal/fr/proxy-list.png',
+      adspowerCopyProxy: '/images/adspower/fr/01-iproyal-copier-proxy.png',
+      adspowerOpenProxies: '/images/adspower/fr/02-section-proxies.png',
+      adspowerPasteProxy: '/images/adspower/fr/03-coller-proxy.png',
+      adspowerImportedProxy: '/images/adspower/fr/04-proxy-importe.png',
+      adspowerLinkProfile: '/images/adspower/fr/05-lier-proxy-profil.png',
+      adspowerChooseSaved: '/images/adspower/fr/06-choisir-saved-proxy.png',
+      adspowerVerifyProxy: '/images/adspower/fr/07-verification-proxy.png'
     })
   })
 });
