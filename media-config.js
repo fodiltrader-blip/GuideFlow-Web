9b78eb227265b7721039c85c9099274357a3c737
fodiltrader-blip <fodiltrader@gmail.com>
2026-09-16 14:09:05 +0100

    Configure localized IPRoyal R2 screenshots

--- media-config.js
+++ media-config.js
@@ -2,8 +2,15 @@ window.GuideFlowMedia = Object.freeze({
   provider: 'cloudflare-r2',
   baseUrl: 'https://pub-6a3d51e9b5fa4255945d84fee5915dbf.r2.dev',
   assets: Object.freeze({
-    iproyalOverview: '/images/iproyal/residential-interface.jpg',
-    iproyalSettings: '/images/iproyal/proxy-settings.jpg',
-    iproyalList: '/images/iproyal/proxy-list.jpg'
+    ar: Object.freeze({
+      iproyalOverview: '/images/iproyal/ar/residential-interface.png',
+      iproyalSettings: '/images/iproyal/ar/proxy-settings.png',
+      iproyalList: '/images/iproyal/ar/proxy-list.png'
+    }),
+    fr: Object.freeze({
+      iproyalOverview: '/images/iproyal/fr/residential-interface.png',
+      iproyalSettings: '/images/iproyal/fr/proxy-settings.png',
+      iproyalList: '/images/iproyal/fr/proxy-list.png'
+    })
   })
 });
