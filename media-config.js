966413a8f4ec598612fb638db5f1117aabaf3fb4
fodiltrader-blip <fodiltrader@gmail.com>
2026-09-16 13:21:23 +0100

    Add centralized R2 media configuration

---
+++ media-config.js
@@ -0,0 +1,9 @@
+window.GuideFlowMedia = Object.freeze({
+  provider: 'cloudflare-r2',
+  baseUrl: 'https://pub-6a3d51e9b5fa4255945d84fee5915dbf.r2.dev',
+  assets: Object.freeze({
+    iproyalOverview: '/images/iproyal/residential-interface.jpg',
+    iproyalSettings: '/images/iproyal/proxy-settings.jpg',
+    iproyalList: '/images/iproyal/proxy-list.jpg'
+  })
+});
